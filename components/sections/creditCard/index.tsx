c897f35478464a5b8d23739268aff9b821756e40
Bernardo <bernardowerty@outlook.com>
2026-01-28 14:19:33 -0300

    tradução da section card

--- components/sections/creditCard/index.tsx
+++ components/sections/creditCard/index.tsx
@@ -1,26 +1,32 @@
+'use client';
+
+
 import { ContainerGRID } from "@/components/containerGRID";
 import Image from "next/image";
 
 
 import iconVerified from "@/assets/icons/icon-green-okay.svg";
+import { useTranslation } from "react-i18next";
 
 
 
-const listing = [
-    "Instant spending notifications across all your payment cards",
-    "Free Lanistar-to-Lanistar payments",
-    "Spend in multiple currencies",
-    "Track your spending"
-]
+
 
 export function SectionCredit(){
+    const {t} = useTranslation();
+
+const items = t(
+  'sectionCredit.listingPay',
+  { returnObjects: true }
+) as string[];
+
     return(
         <section className="py-28">
               <ContainerGRID className="flex items-start gap-48">
-                   <h2 className="text-6xl font-light text-neutral-color-300 max-w-lg"><span className="text-neutral-color-400">LANISTAR</span> PAYMENT CARD</h2>
+                   <h2 className="text-6xl font-light text-neutral-color-300 max-w-lg"><span className="text-neutral-color-400">LANISTAR</span> {t('sectionCredit.title')}</h2>
                    <ul className="space-y-6 max-w-listingSection">
                       {
-                         listing.map((item,index) => (
+                         items.map((item,index) => (
                              <li key={`${item} + ${index}`} className="flex items-start gap-6">
                                 <div className="w-6 h-6 flex items-center justify-center bg-greenDark-900/10 rounded-full">
                                 <Image
